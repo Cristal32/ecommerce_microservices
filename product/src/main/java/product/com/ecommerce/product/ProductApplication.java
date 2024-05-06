ff392ee81f0670ff598cbaf6f7cbd9b83ae1d074
fatibr19 <fatibraikat@gmail.com>
2024-05-06 14:11:00 +0100

    adding eureka server service and updating all the services for be ing eureka clients

--- product/src/main/java/product/com/ecommerce/product/ProductApplication.java
+++ product/src/main/java/product/com/ecommerce/product/ProductApplication.java
@@ -2,9 +2,11 @@ package product.com.ecommerce.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductApplication {
 	
 	public static void main (String[] args) {
